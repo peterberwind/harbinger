9ce703b84ba7db52156c7efb43ed2d0160865c66
peterberwind <peterberwind@gmail.com>
2016-07-05 13:24:05 -0400

    adding bourbon, neat, base

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
+    return gulp.src('_assets/scss/main.scss')
         .pipe(sass({
             includePaths: ['scss'],
             onError: browserSync.notify
@@ -56,7 +56,7 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch('_assets/scss/*.scss', ['sass']);
     gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
