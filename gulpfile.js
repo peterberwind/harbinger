00b7ecbd89e6958011f241048fbedefa3a2f5e61
peterberwind <peterberwind@gmail.com>
2016-07-06 17:52:30 -0400

    adding js concat and source maps for sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
+var concat      = require('gulp-concat');
+var sourcemaps  = require('gulp-sourcemaps');
 
 // TODO: Add Growl Notifications
-// TODO: Add JS Minification + Concatination
+// TODO: Add JS Minification
 // TODO: Add JS Hint
 // TODO: Add Scss Source Maps
 // TODO: Add Css Minification
@@ -45,18 +47,33 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 });
 
 /**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ * Contact+ Compile JS from _assets/js into both _site/js (for live injecting) and site (for future jekyll builds)
+ */
+
+ gulp.task('scripts', function() {
+  return gulp.src('_assets/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('_site/js'))
+    .pipe(gulp.dest('js'));
+});
+
+
+
+/**
+ * Compile files from _assets into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
     return gulp.src('_assets/scss/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: ['scss'],
             onError: browserSync.notify
         }))
+        .pipe(sourcemaps.write())
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('_site/css'));
 });
 
 /**
@@ -65,6 +82,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('_assets/scss/**/*.scss', ['sass']);
+    gulp.watch('_assets/js/**/*.js', ['scripts']);
     gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
 });
 
